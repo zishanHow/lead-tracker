a8ec9d161a68cb93956d831e78ee4a2b2000877e
zishanHow <111298322+zishanHow@users.noreply.github.com>
2022-09-17 21:49:38 +0600

    Update index.js
    setitem in localStorage and getitem from localStorage.

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
+console.log(leadsFromLocalStorage)
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
